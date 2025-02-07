2d3bea2e9809f38cfa9d460c2fa74c9f6d6ea857
delliott18 <148833506+delliott18@users.noreply.github.com>
2025-02-07 00:38:10 -0700

    Created AdminCommandsPage.java
    Page for all of the admin commands ex:
    
    -Delete Member
    -Change role
    -view members list & info
    -etc

---
+++ HW1/src/application/AdminCommandsPage.java
@@ -0,0 +1,47 @@
+package application;
+
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+/**
+ * AdminPage class represents the user interface for the admin user.
+ * This page displays a simple welcome message for the admin.
+ */
+
+public class AdminHomePage {
+	/**
+     * Displays the admin page in the provided primary stage.
+     * @param primaryStage The primary stage where the scene will be displayed.
+     */
+    public void show(Stage primaryStage) {
+    	VBox layout = new VBox();
+    	
+	    layout.setStyle("-fx-alignment: center; -fx-padding: 20;");
+	    
+	    // label to display the welcome message for the admin
+	    Label adminLabel = new Label("Hello, Admin!");
+	    
+	    adminLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+	    
+	 // button to proceed to the main admin screen
+        Button continueButton = new Button("Continue");
+        continueButton.setOnAction(e -> {
+            // call another class that shows the main admin screen
+           AdminCommandsPage mainPage = new AdminCommandsPage();
+            mainPage.show(primaryStage);
+        });
+
+	    layout.getChildren().addAll(adminLabel, continueButton);
+	    Scene adminScene = new Scene(layout, 800, 400);
+
+	    // Set the scene to primary stage
+	    primaryStage.setScene(adminScene);
+	    primaryStage.setTitle("Admin Page");
+	    
+    }
+    
+    
+}
