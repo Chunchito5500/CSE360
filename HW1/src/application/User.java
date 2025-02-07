45f3da9a66fd0d2cb7c553ab47f07cf10b1d451d
delliott18 <148833506+delliott18@users.noreply.github.com>
2025-02-07 00:07:09 -0700

    Update for some Admin functions, User.java
    Added email and name fields along with getting and setters

--- HW1/src/application/User.java
+++ HW1/src/application/User.java
@@ -8,6 +8,8 @@ public class User {
     private String userName;
     private String password;
     private String role;
+    private String email;
+    private String name;
 
     // Constructor to initialize a new User object with userName, password, and role.
     public User( String userName, String password, String role) {
@@ -20,8 +22,18 @@ public class User {
     public void setRole(String role) {
     	this.role=role;
     }
+    
+    public void setName(String name) {
+    	this.name = name;
+    }
+    
+    public void setEmail(String email) {
+    	this.email = email;
+    }
 
     public String getUserName() { return userName; }
     public String getPassword() { return password; }
     public String getRole() { return role; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
 }
